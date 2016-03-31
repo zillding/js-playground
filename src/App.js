ff3fbae53327ad6c7850bb12eae069a4b3503b89
Zill Ding <zilldzy@gmail.com>
2016-03-31 15:42:30 +0800

    Update run button position

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ const editorContainerStyle = {
   position: 'relative'
 }
 
+const buttonStyle = {
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  zIndex: 9999
+}
+
 class App extends Component {
   render() {
     const { editorContent, onEditorContentChange } = this.props
@@ -29,9 +36,13 @@ class App extends Component {
         style={containerStyle}>
         <Item flex={0}>
           <div>Playground</div>
-          <button onClick={() => evalText(editorContent)}>run</button>
         </Item>
         <Item style={editorContainerStyle}>
+          <button
+            style={buttonStyle}
+            onClick={() => evalText(editorContent)}>
+            run
+          </button>
           <Editor onChange={onEditorContentChange} />
         </Item>
       </Flex>
