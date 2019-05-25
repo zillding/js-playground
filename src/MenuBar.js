11922d61c7430b4b939876c0fb9ef11bf61740f2
Zill Ding <zilldzy@gmail.com>
2019-05-25 20:15:11 +0800

    Clean up menu bar component

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -9,86 +9,67 @@ import {
   addLibrary
 } from './actions';
 
-import Seperator from './MenuBarSeperator';
 import DefaultLibraries from './DefaultLibraries';
-import AddLibrary from './AddLibrary';
 import VimToggle from './VimToggle';
 import LibraryLoadingIndicator from './LibraryLoadingIndicator';
 import LibraryListPanelToggle from './LibraryListPanelToggle';
 
-const itemStyle = {
-  flexShrink: 0,
-  margin: 5
-};
-const rightItemStyle = Object.assign({}, itemStyle, {
-  marginLeft: 'auto'
-});
+const Seperator = () => <span style={{ margin: '0 4px' }}>|</span>;
 
-const MenuBar = ({
+function MenuBar({
   defaultLibraries,
   libraryListIsOpen,
   editorVimModeEnabled,
   libraries,
   onToggleLibraryList,
-  onAddDefaultLibraryRequest,
-  onResetDefaultLibrariesRequest,
   onToggleVimMode,
   onAddLibrary
-}) => (
-  <div style={{ display: 'flex', alignItems: 'center', overflow: 'auto' }}>
-    <div style={itemStyle}>
+}) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        overflow: 'auto',
+        padding: 4
+      }}
+    >
       <span>JS Playground</span>
-    </div>
-    <Seperator />
-    <div style={itemStyle}>
-      <DefaultLibraries data={defaultLibraries} onSelect={onAddLibrary} />
-    </div>
-    <Seperator />
-    <div style={itemStyle}>
-      <AddLibrary
-        onAdd={onAddLibrary}
-        onAddDefaultRequest={onAddDefaultLibraryRequest}
-        onResetDefaultRequest={onResetDefaultLibrariesRequest}
-      />
-    </div>
-    <Seperator />
-    <div style={itemStyle}>
+      <Seperator />
       <VimToggle on={editorVimModeEnabled} onToggle={onToggleVimMode} />
+      <Seperator />
+      <button>Add Library</button>
+      <Seperator />
+      <DefaultLibraries data={defaultLibraries} onSelect={onAddLibrary} />
+      {libraries.length > 0 && (
+        <div style={{ marginLeft: 'auto' }}>
+          <LibraryLoadingIndicator libraries={libraries} />
+          <LibraryListPanelToggle
+            libraryListIsOpen={libraryListIsOpen}
+            onToggleLibraryList={onToggleLibraryList}
+          />
+        </div>
+      )}
     </div>
-    {libraries.length > 0 ? (
-      <div style={rightItemStyle}>
-        <LibraryLoadingIndicator libraries={libraries} />
-        <LibraryListPanelToggle
-          libraryListIsOpen={libraryListIsOpen}
-          onToggleLibraryList={onToggleLibraryList}
-        />
-      </div>
-    ) : null}
-  </div>
-);
+  );
+}
 
-const mapStateToProps = state => {
-  return {
-    defaultLibraries: state.defaultLibraries,
-    libraryListIsOpen: state.libraryListIsOpen,
-    editorVimModeEnabled: state.editorVimModeEnabled,
-    libraries: state.libraries
-  };
-};
+const mapStateToProps = state => ({
+  defaultLibraries: state.defaultLibraries,
+  libraryListIsOpen: state.libraryListIsOpen,
+  editorVimModeEnabled: state.editorVimModeEnabled,
+  libraries: state.libraries
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddLibrary: library => dispatch(addLibrary(library)),
-    onToggleLibraryList: () => dispatch(toggleLibraryList()),
-    onAddDefaultLibraryRequest: () => dispatch(toggleAddDefaultLibrary(true)),
-    onResetDefaultLibrariesRequest: () => dispatch(resetDefaultLibraries()),
-    onToggleVimMode: () => dispatch(toggleEditorVimMode())
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  onAddLibrary: library => dispatch(addLibrary(library)),
+  onToggleLibraryList: () => dispatch(toggleLibraryList()),
+  onAddDefaultLibraryRequest: () => dispatch(toggleAddDefaultLibrary(true)),
+  onResetDefaultLibrariesRequest: () => dispatch(resetDefaultLibraries()),
+  onToggleVimMode: () => dispatch(toggleEditorVimMode())
+});
 
-const MenuBarComponent = connect(
+export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(MenuBar);
-
-export default MenuBarComponent;
