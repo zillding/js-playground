83a07cb2455ed0450898dc24b06469ee837fcfa5
Zill Ding <zilldzy@gmail.com>
2019-05-25 23:15:34 +0800

    Update welcome text

--- src/lib/welcome.txt.js
+++ src/lib/welcome.txt.js
@@ -3,10 +3,13 @@ export default `
 
 // Made with ❤ By Zill Ding at https://github.com/zillding/js-playground
 
-// shortcuts:
-// cmd+enter: run code
-// cmd+k:     clear console
-// cmd+s:     format code
+// key bindings:
+// |    mac    |     pc     |                |
+// |:---------:|:----------:|----------------|
+// | cmd+enter | ctrl+enter | run code       |
+// | cmd+k     | ctrl+k     | clear console  |
+// | cmd+s     | ctrl+s     | format code    |
+// | cmd+o     | ctrl+o     | search library |
 
 alert('This is an AWESOME app!')
 console.log('Happy coooooding') // open developer console to view result
