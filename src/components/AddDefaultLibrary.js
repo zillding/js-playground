6323ab5e4cf4829d7dc8ad7388804533d24ce3d5
Zill Ding <zilldzy@gmail.com>
2019-05-25 14:13:03 +0800

    Separate prop types package

--- src/components/AddDefaultLibrary.js
+++ src/components/AddDefaultLibrary.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { isUri } from 'valid-url';
 
 const containerStyle = {
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,6 @@
 // global: ace
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import debounce from 'debounce';
 
 const editorStyle = {
