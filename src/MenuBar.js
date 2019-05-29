8784a7af60760c05563a853e0a1eaf0bbb6dc91d
Zill Ding <zilldzy@gmail.com>
2019-05-29 19:00:13 +0800

    Add key bindings modal and update styling

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import KeyBindingsModal from './KeyBindingsModal';
 
 const Seperator = () => <span style={{ margin: '0 4px' }}>|</span>;
 
 function MenuBar({ editorVimModeEnabled, onToggleVimMode }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div
       style={{
@@ -14,7 +18,7 @@ function MenuBar({ editorVimModeEnabled, onToggleVimMode }) {
     >
       <span>JS Playground</span>
       <Seperator />
-      <label style={{ cursor: 'pointer' }}>
+      <label style={{ display: 'flex', cursor: 'pointer' }}>
         <input
           type="checkbox"
           style={{ cursor: 'pointer' }}
@@ -23,6 +27,21 @@ function MenuBar({ editorVimModeEnabled, onToggleVimMode }) {
         />
         {editorVimModeEnabled ? 'Vim On' : 'Vim Off'}
       </label>
+
+      <button
+        style={{ marginLeft: 'auto' }}
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+      >
+        ?
+      </button>
+      <KeyBindingsModal
+        isOpen={isModalOpen}
+        onRequestClose={() => {
+          setIsModalOpen(false);
+        }}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import './index.css';
 
 render(<App />, document.getElementById('root'));
 
