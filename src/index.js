5e64623287e52ea61a8270eb942693be8e6347a4
Zill Ding <zilldzy@gmail.com>
2023-02-12 15:11:13 +0800

    Update react initialization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import Modal from 'react-modal';
 
 import * as serviceWorker from './serviceWorker';
@@ -9,7 +9,14 @@ import './index.css';
 // bind modal to app root: http://reactcommunity.org/react-modal/accessibility/
 Modal.setAppElement('#root');
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
